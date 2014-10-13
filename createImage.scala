0ea6836c15004972eb9224e4b8182b96fb79cbac
Michael Zhang <zhangmichael@berkeley.edu>
2014-10-13 16:43:02 -0700

    random colors

--- createImage.scala
+++ createImage.scala
@@ -1,5 +1,6 @@
 import java.awt.image.BufferedImage
 import java.lang.StringBuilder
+import util.Random
 
 object createImage {
 	def main(args: Array[String]) = {
@@ -8,10 +9,11 @@ object createImage {
 
 	def create(color: Int, width: Int, height: Int, location: String, random: Boolean) {
 		val img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB)
-		if (random) {
-			//TODO: Implement random RGB values
-		}
+
 		for (x <- width - 1; y <- height - 1) {
+			if (random) {
+				color = Random.nextInt(256)
+			}
 			img.setRGB(x, y, color)
 		}
 		try {
